2ed1a113089d628cf67ce1d2974ea4a38e56c89c
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-27 09:08:09 +0700

    Link Component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>login page</h1>
+      <Link to="/register">Register Page</Link>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <h1>register page</h1>
+      <Link to="/login">Login Page</Link>
+    </div>
+  );
 };
 
 export default Register;
