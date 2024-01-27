c58789d673c0fe8d13fc9a5aeaba043f72f8c59b
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-27 09:41:52 +0700

    Setup Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/">back home</Link>
+    </div>
+  );
 };
 
 export default Error;
