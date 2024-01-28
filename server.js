027d95e9bca7aa5ef3ccdfed50a394a972e8d2f1
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-28 13:08:59 +0700

    JSON middleware

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
 });
 
 app.listen(5100, () => {
