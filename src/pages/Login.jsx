c5359e6ef068fe8a27dd5f64d82fbd24f151f29c
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-28 09:11:46 +0700

    Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FormRow, Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 
 const Login = () => {
   return (
-    <div>
-      <h1>login page</h1>
-      <Link to="/register">Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
