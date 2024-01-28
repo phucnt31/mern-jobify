e67b91f5c92d636ceb2a3847f24bb896933d8a13
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-28 10:29:13 +0700

    SmallSidebar functionality

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -7,13 +7,17 @@ import links from "../utils/links";
 import { useDashboardContext } from "../pages/DashboardLayout";
 
 const SmallSidebar = () => {
-  const data = useDashboardContext();
+  const { showSidebar, toggleSidebar } = useDashboardContext();
 
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
         <div className="content">
-          <button type="button" className="close-btn">
+          <button type="button" onClick={toggleSidebar} className="close-btn">
             <FaTimes />
           </button>
           <header>
@@ -23,7 +27,13 @@ const SmallSidebar = () => {
             {links.map((link) => {
               const { text, path, icon } = link;
               return (
-                <NavLink to={path} key={text} className="nav-link">
+                <NavLink
+                  to={path}
+                  key={text}
+                  className="nav-link"
+                  onClick={toggleSidebar}
+                  end
+                >
                   <span className="icon">{icon}</span>
                   {text}
                 </NavLink>
