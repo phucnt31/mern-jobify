f79992f36b52a195e8b2dbf5dd3fc430a0c30011
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-28 13:42:30 +0700

    Get All Jobs

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
+import { nanoid } from "nanoid";
 
 dotenv.config();
 const app = express();
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -17,6 +23,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
