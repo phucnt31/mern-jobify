aa47076ffaeb1fc26ff56c0e12ffd235a2c77bf2
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-28 12:50:56 +0700

    Express and Nodemon

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("server is working");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(5100, () => {
+  console.log("Server running");
+});
