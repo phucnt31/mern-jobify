728c40c5ef03433aceaa57c8a90f9f23b50d2da6
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-29 13:18:25 +0700

    Validate job input

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -6,10 +6,15 @@ import {
   getJob,
   updateJob,
 } from "../controllers/jobController.js";
+import { validateJobInput } from "../middleware/validationMiddleware.js";
 
 const router = Router();
 
-router.route("/").get(getAllJobs).post(createJob);
-router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
+router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route("/:id")
+  .get(getJob)
+  .patch(validateJobInput, updateJob)
+  .delete(deleteJob);
 
 export default router;
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import jobRouter from "./routes/jobRouter.js";
 
 // middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
-import { validateTest } from "./middleware/validationMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -18,11 +17,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
-
-app.post("/api/v1/test", validateTest, (req, res) => {
-  const { name } = req.body;
-  res.json({ msg: `hello ${name}` });
-});
 
 app.use("/api/v1/jobs", jobRouter);
 
