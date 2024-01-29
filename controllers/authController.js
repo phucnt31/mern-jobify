5c10137876164ab3368037664558f28a79692700
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-29 18:03:12 +0700

    Login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,29 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import { hashedPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
-  req.body.password = await hashedPassword(req.body.password);
+  req.body.password = await hashPasswordPassword(req.body.password);
 
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    throw new UnauthenticatedError("invalid credentials");
+  }
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("invalid credentials");
+  }
   res.send("login");
 };
