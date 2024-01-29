64d50de3751678b53ac98c25c00e15a5e65b4cb2
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-29 17:02:19 +0700

    User controller and router

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 // middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -19,6 +20,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 
 app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
