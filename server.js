3c4a8982bd46bfb300883e2e83319a02fc02831b
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-30 09:05:46 +0700

    Setup authenticate user

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import authRouter from "./routes/authRouter.js";
 
 // middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -19,7 +20,7 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json());
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
