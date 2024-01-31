6d4f0a5d040c42b9f15afdbaaa7f0242039cdae0
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-31 15:30:19 +0700

    Delete Job action

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { action as addJobAction } from "./pages/AddJob";
 import { action as editJobAction } from "./pages/EditJob";
+import { action as deleteJobAction } from "./pages/DeleteJob";
 
 // loader
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
@@ -86,6 +87,10 @@ const router = createBrowserRouter([
             element: <EditJob />,
             loader: editJobLoader,
             action: editJobAction,
+          },
+          {
+            path: "delete-job/:id",
+            action: deleteJobAction,
           },
         ],
       },
