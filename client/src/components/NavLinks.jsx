2aa195fe4cef2eb8798c1e1778f36f6a209790ee
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-31 15:43:41 +0700

    Admin page setup

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -4,12 +4,14 @@ import { NavLink } from "react-router-dom";
 import React from "react";
 
 const NavLinks = ({ isBigSidebar }) => {
-  const { toggleSidebar } = useDashboardContext();
+  const { toggleSidebar, user } = useDashboardContext();
 
   return (
     <div className="nav-links">
       {links.map((link) => {
         const { text, path, icon } = link;
+        const { role } = user;
+        if (path === "admin" && role !== "admin") return;
         return (
           <NavLink
             to={path}
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+import { useLoaderData, redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { toast } from "react-toastify";
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get("/users/admin/app-stats");
+    return response.data;
+  } catch (error) {
+    toast.error("You are not authorized to view this page");
+    return redirect("/dashboard");
+  }
+};
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const { users, jobs } = useLoaderData();
+
+  return <Wrapper>Admin</Wrapper>;
 };
 
 export default Admin;
