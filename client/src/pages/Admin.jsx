f96d8b64b989f828a87f9a0de1fa208a59ed38d9
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-31 15:48:46 +0700

    Admin page complete

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -4,6 +4,7 @@ import { useLoaderData, redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
+import { StatItem } from "../components";
 
 export const loader = async () => {
   try {
@@ -18,7 +19,24 @@ export const loader = async () => {
 const Admin = () => {
   const { users, jobs } = useLoaderData();
 
-  return <Wrapper>Admin</Wrapper>;
+  return (
+    <Wrapper>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
+    </Wrapper>
+  );
 };
 
 export default Admin;
