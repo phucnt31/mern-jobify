78fd980cf2dd8a9749ef2230a164e44a5ab1071c
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-01 10:31:10 +0700

    Stats page setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,7 @@ import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { loader as adminLoader } from "./pages/Admin";
+import { loader as statsLoader } from "./pages/Stats";
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -70,6 +71,7 @@ const router = createBrowserRouter([
           {
             path: "stats",
             element: <Stats />,
+            loader: statsLoader,
           },
           {
             path: "all-jobs",
