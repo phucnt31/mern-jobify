01b57c3e6fcfe0818bf13d9ca93936a107208002
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-01 14:14:25 +0700

    Fix mismatch method

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
-app.get("*", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public", "index.html"));
 });
 
