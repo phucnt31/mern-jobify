4893ceff6306ca55468727be6cc27013d6cd79e9
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-01 14:27:39 +0700

    Reverse change

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
-app.use("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public", "index.html"));
 });
 
