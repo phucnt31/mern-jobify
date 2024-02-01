17e7b052297f50ca329c7b40f1e0395c6d2ea75d
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-01 11:29:04 +0700

    Pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -30,8 +30,21 @@ export const getAllJobs = async (req, res) => {
   };
   const sortKey = sortOptions[sort] || sortOptions.newest;
 
-  const jobs = await JobModel.find(queryObject).sort(sortKey);
-  res.status(StatusCodes.OK).json({ jobs });
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await JobModel.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+  const totalJobs = await JobModel.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 export const createJob = async (req, res) => {
