03b42bd6bdffde67960e9061cc4b62bf9a2f976a
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-02 11:08:05 +0700

    React Query in Stats loader

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -4,27 +4,23 @@ import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
-export const loader = async () => {
+const statsQuery = {
+  queryKey: ["stats"],
+  queryFn: async () => {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  const data = await queryClient.ensureQueryData(statsQuery);
   return null;
-  const response = await customFetch.get("/jobs/stats");
-  return response.data;
 };
 
 const Stats = () => {
-  // const { defaultStats, monthlyApplications } = useLoaderData();
-  const { isLoading, isError, data } = useQuery({
-    queryKey: ["stats"],
-    queryFn: () => customFetch.get("/jobs/stats"),
-  });
+  const { data } = useQuery(statsQuery);
+  const { defaultStats, monthlyApplications } = data;
 
-  if (isLoading) {
-    return <h4>Loading...</h4>;
-  }
-  if (isError) {
-    return <h4>Error...</h4>;
-  }
-
-  const { defaultStats, monthlyApplications } = data.data;
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
