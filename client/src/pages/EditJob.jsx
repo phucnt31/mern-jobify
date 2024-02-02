54c25aa31d94f84ff14c088a92ee946753fd8840
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-02 12:37:45 +0700

    React Query for edit job

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -6,16 +6,29 @@ import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
 import { Form, redirect } from "react-router-dom";
 import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
+import { useQuery } from "@tanstack/react-query";
 
-export const loader = async ({ params }) => {
-  try {
-    const { data } = await customFetch.get(`/jobs/${params.id}`);
-    return data;
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return redirect("/dashboard/all-jobs");
-  }
+const singleJobQuery = (id) => {
+  return {
+    queryKey: ["job", id],
+    queryFn: async () => {
+      const { data } = await customFetch.get(`/jobs/${id}`);
+      return data;
+    },
+  };
 };
+
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    try {
+      await queryClient.ensureQueryData(singleJobQuery(params.id));
+      return params.id;
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return redirect("/dashboard/all-jobs");
+    }
+  };
 
 export const action =
   (queryClient) =>
@@ -25,6 +38,7 @@ export const action =
     try {
       await customFetch.patch(`/jobs/${params.id}`, data);
       queryClient.invalidateQueries(["jobs"]);
+      queryClient.invalidateQueries(["job", params.id]);
       toast.success("Job edited successfully");
       return redirect("/dashboard/all-jobs");
     } catch (error) {
@@ -34,7 +48,10 @@ export const action =
   };
 
 const EditJob = () => {
-  const { job } = useLoaderData();
+  const id = useLoaderData();
+  const {
+    data: { job },
+  } = useQuery(singleJobQuery(id));
 
   return (
     <Wrapper>
