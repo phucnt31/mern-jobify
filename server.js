877050dbdf26bff0378c11c446314eecfbf40352
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-02 09:38:13 +0700

    Build front-end programmatically

--- server.js
+++ server.js
@@ -36,14 +36,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
